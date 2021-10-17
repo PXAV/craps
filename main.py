cc8864d2ae2f342706500d99f83ef43fcb9a6ea0
PXAV <pxav.studios@gmail.com>
2021-10-17 12:19:43 +0200

    Themes and prefs are now loaded on app startup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from ui.window import Window
 
+from storage.user_preferences import load_preferences
+from ui.theme.theme_repository import load_all_themes
+
 if __name__ == '__main__':
+    load_all_themes()
+    load_preferences()
+
     main_window = Window()
     main_window.open()
