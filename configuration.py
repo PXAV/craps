faf2f60954e7ae904deb55aff0a15153b2175d9e
PXAV <pxav.studios@gmail.com>
2021-10-17 16:37:03 +0200

    add font paths to configuration.py

--- configuration.py
+++ configuration.py
@@ -1,1 +1,4 @@
 preference_file = 'resources/user_preferences.toml'
+normal_font = 'resources/font/GothamMedium.ttf'
+thin_font = 'resources/font/GothamLight.ttf'
+bold_font = 'resources/font/GothamBold.ttf'
