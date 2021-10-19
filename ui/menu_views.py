5a3be4a35bbdeac2709e8ab3845a2a97b03eba79
PXAV <pxav.studios@gmail.com>
2021-10-19 14:53:19 +0200

    Add game title to main menu

--- ui/menu_views.py
+++ ui/menu_views.py
@@ -12,27 +12,39 @@ from ui.theme.theme_properties import ThemeProperty
 def open_main_menu(window: Window):
     window.clear_widgets()
 
-    play_button = CrapsButton(master=window,width=300, height=40,
+    title_label = CrapsButton(master=window, width=300, height=40,
+                              text="CRAPS", text_type="bold", text_size=26,
+                              opaque=False)
+    title_label.show_grid(column=1, row=0, columnspan=2, pady=((window.get_height() / 8), 0))
+
+    play_button = CrapsButton(master=window, width=300, height=40,
                               text="PLAY!",
                               callback=lambda event: print("clicked"))
-    play_button.show_grid(column=1, row=0, columnspan=2, pady=(20, 10), padx=(40, 0))
+    play_button.show_grid(column=1, row=1, columnspan=2, pady=(20, 10), padx=(40, 0))
 
     stats_button = CrapsButton(master=window, width=300, height=40,
                                text="STATISTICS")
-    stats_button.show_grid(column=1, row=1, columnspan=2, pady=10, padx=(40, 0))
+    stats_button.show_grid(column=1, row=2, columnspan=2, pady=10, padx=(40, 0))
 
     settings_button = CrapsButton(master=window, width=300, height=40,
                                   text="SETTINGS",
                                   callback=lambda event: open_settings_menu(window))
-    settings_button.show_grid(column=1, row=2, columnspan=2, pady=10, padx=(40, 0))
+    settings_button.show_grid(column=1, row=3, columnspan=2, pady=10, padx=(40, 0))
 
     quit_button = CrapsButton(master=window, width=300, height=40,
                               text="QUIT",
                               callback=lambda event: exit(0))
-    quit_button.show_grid(column=1, row=3, columnspan=2, pady=10, padx=(40, 0))
+    quit_button.show_grid(column=1, row=4, columnspan=2, pady=10, padx=(40, 0))
 
 
 def open_settings_menu(window: Window):
     window.clear_widgets()
+
+
+
+    play_button = CrapsButton(master=window, width=300, height=40,
+                              text="PLAY!",
+                              callback=lambda event: print("clicked"))
+    play_button.show_grid(column=1, row=0, columnspan=2, pady=(20, 10), padx=(40, 0))
 
     window.update()
