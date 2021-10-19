2993e60729b6c0c7991e9fce43d1c143d6448bdd
PXAV <pxav.studios@gmail.com>
2021-10-19 12:34:45 +0200

    Main menu is now opened in main.py

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from ui.window import Window
 
 from storage.user_preferences import load_preferences
 from ui.theme.theme_repository import load_all_themes
+from ui.main_menu import open_main_menu
+
 
 if __name__ == '__main__':
     load_all_themes()
     load_preferences()
 
-    main_window = Window()
+    main_window = Window(screen_name="Craps")
+
+    open_main_menu(main_window)
+
     main_window.open()
