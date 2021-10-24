6db4a393ba8d4d3defb13dd03c9a152a43f43cbb
PXAV <pxav.studios@gmail.com>
2021-10-24 15:33:23 +0200

    Add methods to customize the grid of a window

--- ui/window.py
+++ ui/window.py
@@ -15,7 +15,23 @@ class Window(Tk):
         self.custom_grid_size = 0
 
     def open(self):
-        self.root.mainloop()
+        self.mainloop()
+
+    def clear_widgets(self):
+        widget_list = self.grid_slaves()
+        for widget in widget_list:
+            widget.destroy()
+
+    def configure_grid(self, grid_size):
+        self.custom_grid_size = grid_size
+        for i in range(grid_size):
+            self.rowconfigure(i, weight=1)
+            self.columnconfigure(i, weight=1)
+
+    def reset_custom_grid(self):
+        for i in range(self.custom_grid_size):
+            self.rowconfigure(i, weight=0)
+            self.columnconfigure(i, weight=0)
 
     def get_height(self) -> int:
         return self.height
