056deb0acc651b567b14d5b78d864724e467d3d3
PXAV <pxav.studios@gmail.com>
2021-10-24 17:24:09 +0200

    Add new theme properties needed for dice indicator

--- ui/theme/theme_properties.py
+++ ui/theme/theme_properties.py
@@ -3,9 +3,12 @@ from enum import Enum
 
 class ThemeProperty(Enum):
     PRIMARY_BACKGROUND = "colors.primary_background"
+    SECONDARY_BACKGROUND = "colors.secondary_background"
 
     PRIMARY_TEXT = 'colors.primary_text'
     SECONDARY_TEXT = 'colors.secondary_text'
+
+    DICE_INDICATOR_ACTIVE = "colors.dice_indicator_active"
 
     PRIMARY_BUTTON = 'colors.primary_button'
     SECONDARY_BUTTON = 'colors.secondary_button'
