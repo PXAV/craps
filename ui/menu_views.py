7a3cd9216ac213129931fa67ad95fecbde5f7463
PXAV <pxav.studios@gmail.com>
2021-10-30 14:51:28 +0200

    Implement rules page in main menu

--- ui/menu_views.py
+++ ui/menu_views.py
@@ -30,7 +30,8 @@ def open_main_menu(window: Window):
     stats_button.show_grid(column=1, row=2, columnspan=2, pady=10, padx=(40, 0))
 
     rules_button = CrapsButton(master=window, width=300, height=40,
-                               text="RULES")
+                               text="RULES",
+                               callback=lambda event: open_rules_menu(window))
     rules_button.show_grid(column=1, row=3, columnspan=2, pady=10, padx=(40, 0))
 
     settings_button = CrapsButton(master=window, width=300, height=40,
@@ -40,16 +41,42 @@ def open_main_menu(window: Window):
 
     quit_button = CrapsButton(master=window, width=300, height=40,
                               text="QUIT",
+                              primary=False,
                               callback=lambda event: exit(0))
     quit_button.show_grid(column=1, row=5, columnspan=2, pady=10, padx=(40, 0))
 
     window.update()
 
 
-def open_settings_menu(window: Window):
+def open_rules_menu(window: Window):
     window.clear_widgets()
 
-    window.configure_grid(10)
+    title_label = CrapsButton(master=window, width=300, height=40,
+                              text="RULES", text_type="bold", text_size=26,
+                              opaque=False)
+    title_label.show_grid(column=1, row=0, columnspan=2, pady=((window.get_height() / 8), 0))
+
+    __multiline_text(window, [
+        "You start with two dices",
+        " - If their sum is 7 or 11, you win",
+        " - If their sum is 2, 3 or 12, you lose",
+        "For any other case, you will throw the dice infinitely, till",
+        "1. The sum is equal to 7: You lose",
+        "2. The sum is equal to your first sum: You win",
+    ], column=1, start_row=1)
+
+    back_button = CrapsButton(master=window, width=300, height=40,
+                              text="Understood",
+                              callback=lambda event: open_main_menu(window))
+    back_button.show_grid(column=1, row=9,
+                          padx=(window.get_width() / 20, 0),
+                          pady=(0, window.get_height() / 8))
+
+    window.update()
+
+
+def open_settings_menu(window: Window):
+    window.clear_widgets()
 
     CrapsButton(master=window,
                 height=40, width=200,
@@ -66,3 +93,15 @@ def open_settings_menu(window: Window):
                           pady=(0, window.get_height() / 12))
 
     window.update()
+
+
+def __multiline_text(window: Window, text_lines: list, column: int = 1, start_row: int = 1):
+    index = 0
+    for line in text_lines:
+        CrapsButton(master=window,
+                    height=33, width=int(window.get_width() / 2),
+                    text_size=16,
+                    text_type="normal",
+                    opaque=False,
+                    text=line).show_grid(column=column, row=start_row + index)
+        index += 1
